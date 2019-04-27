c5e588b73433ab2d09820eacd7576becb1302e88
Behrouz <bkhezry@gmail.com>
2019-04-27 18:15:17 +0430

    add get multiple days weather function to service.

--- app/src/main/java/com/github/bkhezry/weather/service/ApiService.java
+++ app/src/main/java/com/github/bkhezry/weather/service/ApiService.java
@@ -1,6 +1,7 @@
 package com.github.bkhezry.weather.service;
 
 import com.github.bkhezry.weather.model.currentweather.CurrentWeatherResponse;
+import com.github.bkhezry.weather.model.daysweather.MultipleDaysWeatherResponse;
 import com.github.bkhezry.weather.model.fivedayweather.FiveDayResponse;
 
 import io.reactivex.Single;
@@ -24,6 +25,16 @@ public interface ApiService {
       @Query("q") String q,
       @Query("units") String units,
       @Query("lang") String lang,
+      @Query("appid") String appId
+  );
+
+  // Get multiple days weather
+  @GET("forecast/daily")
+  Single<MultipleDaysWeatherResponse> getMultipleDaysWeather(
+      @Query("q") String q,
+      @Query("units") String units,
+      @Query("lang") String lang,
+      @Query("cnt") int dayCount,
       @Query("appid") String appId
   );
 }
