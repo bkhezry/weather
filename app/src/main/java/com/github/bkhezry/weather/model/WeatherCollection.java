f7553e46352a28032f7152072ca737e7eed05328
Behrouz <bkhezry@gmail.com>
2019-04-28 23:24:22 +0430

    add start & end timestamp fields to weather collection class. implement FastAdapter future.

--- app/src/main/java/com/github/bkhezry/weather/model/WeatherCollection.java
+++ app/src/main/java/com/github/bkhezry/weather/model/WeatherCollection.java
@@ -1,13 +1,25 @@
 package com.github.bkhezry.weather.model;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.github.bkhezry.weather.R;
 import com.github.bkhezry.weather.model.daysweather.ListItem;
 import com.github.bkhezry.weather.model.fivedayweather.ListItemHourly;
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.items.AbstractItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class WeatherCollection {
+import butterknife.ButterKnife;
+
+public class WeatherCollection extends AbstractItem<WeatherCollection, WeatherCollection.MyViewHolder> {
   private List<ListItemHourly> listItemHourlies;
   private ListItem listItem;
+  private long timestampStart;
+  private long timestampEnd;
 
   public List<ListItemHourly> getListItemHourlies() {
     return listItemHourlies;
@@ -17,11 +29,73 @@ public class WeatherCollection {
     this.listItemHourlies = listItemHourlies;
   }
 
+  public void addListItemHourlies(ListItemHourly listItem) {
+    if (this.listItemHourlies == null) {
+      this.listItemHourlies = new ArrayList<>();
+    }
+    this.listItemHourlies.add(listItem);
+  }
+
   public ListItem getListItem() {
     return listItem;
   }
 
   public void setListItem(ListItem listItem) {
     this.listItem = listItem;
+  }
+
+  public long getTimestampStart() {
+    return timestampStart;
+  }
+
+  public void setTimestampStart(long timestampStart) {
+    this.timestampStart = timestampStart;
+  }
+
+  public long getTimestampEnd() {
+    return timestampEnd;
+  }
+
+  public void setTimestampEnd(long timestampEnd) {
+    this.timestampEnd = timestampEnd;
+  }
+
+
+  @NonNull
+  @Override
+  public MyViewHolder getViewHolder(@NonNull View v) {
+    return new MyViewHolder(v);
+  }
+
+  @Override
+  public int getType() {
+    return R.id.fastadapter_item_adapter;
+  }
+
+  @Override
+  public int getLayoutRes() {
+    return R.layout.weather_day_item;
+  }
+
+  protected static class MyViewHolder extends FastAdapter.ViewHolder<WeatherCollection> {
+    View view;
+
+
+    MyViewHolder(View view) {
+      super(view);
+      ButterKnife.bind(this, view);
+      this.view = view;
+    }
+
+    @Override
+    public void bindView(@NonNull WeatherCollection item, @NonNull List<Object> payloads) {
+
+    }
+
+    @Override
+    public void unbindView(@NonNull WeatherCollection item) {
+
+    }
+
   }
 }
