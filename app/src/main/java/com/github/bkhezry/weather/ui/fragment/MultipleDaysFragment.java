1e4c33974ba9ff1d61554f577bc28de8fe7c6edd
Behrouz <bkhezry@gmail.com>
2019-05-01 02:12:36 +0430

    init recycler view. get multiple days weather from server & showing in recycler view.

--- app/src/main/java/com/github/bkhezry/weather/ui/fragment/MultipleDaysFragment.java
+++ app/src/main/java/com/github/bkhezry/weather/ui/fragment/MultipleDaysFragment.java
@@ -2,6 +2,7 @@ package com.github.bkhezry.weather.ui.fragment;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,14 +11,35 @@ import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.github.bkhezry.weather.R;
+import com.github.bkhezry.weather.model.daysweather.ListItem;
+import com.github.bkhezry.weather.model.daysweather.MultipleDaysWeatherResponse;
+import com.github.bkhezry.weather.service.ApiService;
+import com.github.bkhezry.weather.utils.ApiClient;
+import com.github.bkhezry.weather.utils.Constants;
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.adapters.ItemAdapter;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class MultipleDaysFragment extends DialogFragment {
-
+  @BindView(R.id.recycler_view)
+  RecyclerView recyclerView;
+  private String cityName = "Saqqez, IR";
+  private String defaultLang = "en";
+  private CompositeDisposable disposable = new CompositeDisposable();
+  private FastAdapter<ListItem> mFastAdapter;
+  private ItemAdapter<ListItem> mItemAdapter;
 
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -25,9 +47,38 @@ public class MultipleDaysFragment extends DialogFragment {
     View view = inflater.inflate(R.layout.fragment_multiple_days,
         container, false);
     ButterKnife.bind(this, view);
+    LinearLayoutManager layoutManager
+        = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+    recyclerView.setLayoutManager(layoutManager);
+    mItemAdapter = new ItemAdapter<>();
+    mFastAdapter = FastAdapter.with(mItemAdapter);
+    recyclerView.setItemAnimator(new DefaultItemAnimator());
+    recyclerView.setAdapter(mFastAdapter);
+    ApiService apiService = ApiClient.getClient(getActivity()).create(ApiService.class);
+    disposable.add(
+        apiService.getMultipleDaysWeather(
+            cityName, Constants.UNITS, defaultLang, 16, Constants.APP_ID)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(new DisposableSingleObserver<MultipleDaysWeatherResponse>() {
+              @Override
+              public void onSuccess(MultipleDaysWeatherResponse response) {
+                handleMultipleDaysResponse(response);
+              }
+
+              @Override
+              public void onError(Throwable e) {
+                Log.e("MainActivity", "onError: " + e.getMessage());
+              }
+            })
+    );
     return view;
   }
 
+  private void handleMultipleDaysResponse(MultipleDaysWeatherResponse response) {
+    mItemAdapter.clear();
+    mItemAdapter.add(response.getList());
+  }
 
   @NonNull
   @Override
@@ -50,5 +101,11 @@ public class MultipleDaysFragment extends DialogFragment {
     if (getFragmentManager() != null) {
       getFragmentManager().popBackStack();
     }
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    disposable.dispose();
   }
 }
