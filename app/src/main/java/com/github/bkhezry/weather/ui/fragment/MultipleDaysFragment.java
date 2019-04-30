b2649027a5c846e5d8450febc4e3c5e5c938aee7
Behrouz <bkhezry@gmail.com>
2019-05-01 01:37:14 +0430

    create multiple days fragment.

---
+++ app/src/main/java/com/github/bkhezry/weather/ui/fragment/MultipleDaysFragment.java
@@ -0,0 +1,54 @@
+package com.github.bkhezry.weather.ui.fragment;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import com.github.bkhezry.weather.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MultipleDaysFragment extends DialogFragment {
+
+
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_multiple_days,
+        container, false);
+    ButterKnife.bind(this, view);
+    return view;
+  }
+
+
+  @NonNull
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    Dialog dialog = super.onCreateDialog(savedInstanceState);
+    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    dialog.setCancelable(true);
+    WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+    lp.copyFrom(dialog.getWindow().getAttributes());
+    lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+    lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+    dialog.getWindow().setAttributes(lp);
+    return dialog;
+  }
+
+
+  @OnClick(R.id.close_button)
+  void close() {
+    dismiss();
+    if (getFragmentManager() != null) {
+      getFragmentManager().popBackStack();
+    }
+  }
+}
