acfc606b9d1d6a7967545ef3df529cd7cd52a223
Behrouz <bkhezry@gmail.com>
2019-05-04 03:20:41 +0430

    create CityInfo class for handle city info.

---
+++ app/src/main/java/com/github/bkhezry/weather/model/CityInfo.java
@@ -0,0 +1,31 @@
+package com.github.bkhezry.weather.model;
+
+public class CityInfo {
+  private int id;
+  private String name;
+  private String country;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+}
