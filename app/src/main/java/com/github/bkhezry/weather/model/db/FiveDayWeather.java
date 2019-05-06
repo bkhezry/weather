629af22d28b1029fcc651100b24a88e4575d2f34
Behrouz <bkhezry@gmail.com>
2019-05-07 04:07:05 +0430

    add FastAdapter functions to FiveDayWeather class.

--- app/src/main/java/com/github/bkhezry/weather/model/db/FiveDayWeather.java
+++ app/src/main/java/com/github/bkhezry/weather/model/db/FiveDayWeather.java
@@ -1,12 +1,34 @@
 package com.github.bkhezry.weather.model.db;
 
-import androidx.annotation.ColorInt;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.view.View;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.github.bkhezry.weather.R;
+import com.github.bkhezry.weather.utils.AppUtil;
+import com.github.bkhezry.weather.utils.Constants;
+import com.google.android.material.card.MaterialCardView;
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.items.AbstractItem;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.objectbox.annotation.Entity;
 import io.objectbox.annotation.Id;
 
 @Entity
-public class FiveDayWeather {
+public class FiveDayWeather extends AbstractItem<FiveDayWeather, FiveDayWeather.MyViewHolder> {
   @Id
   private long id;
   private int dt;
@@ -99,5 +121,73 @@ public class FiveDayWeather {
 
   public void setColorAlpha(int colorAlpha) {
     this.colorAlpha = colorAlpha;
+  }
+
+  @NonNull
+  @Override
+  public MyViewHolder getViewHolder(@NonNull View v) {
+    return new MyViewHolder(v);
+  }
+
+  @Override
+  public int getType() {
+    return R.id.fastadapter_item_adapter;
+  }
+
+  @Override
+  public int getLayoutRes() {
+    return R.layout.weather_day_item;
+  }
+
+  protected static class MyViewHolder extends FastAdapter.ViewHolder<FiveDayWeather> {
+    Context context;
+    View view;
+    @BindView(R.id.day_name_text_view)
+    AppCompatTextView dayNameTextView;
+    @BindView(R.id.temp_text_view)
+    AppCompatTextView tempTextView;
+    @BindView(R.id.min_temp_text_view)
+    AppCompatTextView minTempTextView;
+    @BindView(R.id.max_temp_text_view)
+    AppCompatTextView maxTempTextView;
+    @BindView(R.id.weather_image_view)
+    AppCompatImageView weatherImageView;
+    @BindView(R.id.card_view)
+    MaterialCardView cardView;
+    @BindView(R.id.shadow_view)
+    View shadowView;
+
+    MyViewHolder(View view) {
+      super(view);
+      ButterKnife.bind(this, view);
+      this.view = view;
+      this.context = view.getContext();
+    }
+
+    @Override
+    public void bindView(@NonNull FiveDayWeather item, @NonNull List<Object> payloads) {
+      cardView.setCardBackgroundColor(item.getColor());
+      int[] colors = {
+          Color.TRANSPARENT,
+          item.getColorAlpha(),
+          Color.TRANSPARENT
+      };
+      Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+      calendar.setTimeInMillis(item.getDt() * 1000L);
+      dayNameTextView.setText(Constants.DAYS_OF_WEEK[calendar.get(Calendar.DAY_OF_WEEK) - 1]);
+      tempTextView.setText(String.format(Locale.getDefault(), "%.0f°", item.getTemp()));
+      minTempTextView.setText(String.format(Locale.getDefault(), "%.0f°", item.getMinTemp()));
+      maxTempTextView.setText(String.format(Locale.getDefault(), "%.0f°", item.getMaxTemp()));
+      AppUtil.setWeatherIcon(context, weatherImageView, item.weatherId);
+      GradientDrawable shape = new GradientDrawable(GradientDrawable.Orientation.RIGHT_LEFT, colors);
+      shape.setShape(GradientDrawable.OVAL);
+      shadowView.setBackground(shape);
+    }
+
+    @Override
+    public void unbindView(@NonNull FiveDayWeather item) {
+
+    }
+
   }
 }
