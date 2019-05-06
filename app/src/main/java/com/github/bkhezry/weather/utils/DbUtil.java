1e2d80336ca59e1286d2e3b9fe7598db061a4dbd
Behrouz <bkhezry@gmail.com>
2019-05-07 04:17:12 +0430

    add function to DbUtil class for return FiveDayWeather query.

--- app/src/main/java/com/github/bkhezry/weather/utils/DbUtil.java
+++ app/src/main/java/com/github/bkhezry/weather/utils/DbUtil.java
@@ -1,6 +1,7 @@
 package com.github.bkhezry.weather.utils;
 
 import com.github.bkhezry.weather.model.db.CurrentWeather;
+import com.github.bkhezry.weather.model.db.FiveDayWeather;
 
 import io.objectbox.Box;
 import io.objectbox.query.Query;
@@ -9,5 +10,9 @@ public class DbUtil {
 
   public static Query<CurrentWeather> getCurrentWeatherQuery(Box<CurrentWeather> currentWeatherBox) {
     return currentWeatherBox.query().build();
+  }
+
+  public static Query<FiveDayWeather> getFiveDayWeatherQuery(Box<FiveDayWeather> fiveDayWeatherBox) {
+    return fiveDayWeatherBox.query().build();
   }
 }
