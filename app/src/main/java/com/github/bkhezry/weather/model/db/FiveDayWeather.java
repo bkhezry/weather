3fd05666602f6a5ff8c98ea30bbfded8655b0513
Behrouz <bkhezry@gmail.com>
2019-05-07 03:18:36 +0430

    add FiveDayWeather to local db.

--- app/src/main/java/com/github/bkhezry/weather/model/db/FiveDayWeather.java
+++ app/src/main/java/com/github/bkhezry/weather/model/db/FiveDayWeather.java
@@ -12,4 +12,52 @@ public class FiveDayWeather {
   private double minTemp;
   private double maxTemp;
   private int weatherId;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public int getDt() {
+    return dt;
+  }
+
+  public void setDt(int dt) {
+    this.dt = dt;
+  }
+
+  public double getTemp() {
+    return temp;
+  }
+
+  public void setTemp(double temp) {
+    this.temp = temp;
+  }
+
+  public double getMinTemp() {
+    return minTemp;
+  }
+
+  public void setMinTemp(double minTemp) {
+    this.minTemp = minTemp;
+  }
+
+  public double getMaxTemp() {
+    return maxTemp;
+  }
+
+  public void setMaxTemp(double maxTemp) {
+    this.maxTemp = maxTemp;
+  }
+
+  public int getWeatherId() {
+    return weatherId;
+  }
+
+  public void setWeatherId(int weatherId) {
+    this.weatherId = weatherId;
+  }
 }
