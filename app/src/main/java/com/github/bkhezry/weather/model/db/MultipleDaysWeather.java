14b1e3cddde2203d4d0cc86071c38aa6d987b759
Behrouz <bkhezry@gmail.com>
2019-05-07 05:36:38 +0430

    add FastAdapter functions from MultipleDaysWeather class.

--- app/src/main/java/com/github/bkhezry/weather/model/db/MultipleDaysWeather.java
+++ app/src/main/java/com/github/bkhezry/weather/model/db/MultipleDaysWeather.java
@@ -1,10 +1,30 @@
 package com.github.bkhezry.weather.model.db;
 
+import android.content.Context;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.github.bkhezry.weather.R;
+import com.github.bkhezry.weather.utils.AppUtil;
+import com.github.bkhezry.weather.utils.Constants;
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.items.AbstractItem;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.objectbox.annotation.Entity;
 import io.objectbox.annotation.Id;
 
 @Entity
-public class MultipleDaysWeather {
+public class MultipleDaysWeather extends AbstractItem<MultipleDaysWeather, MultipleDaysWeather.MyViewHolder> {
   @Id
   private long id;
   private int dt;
@@ -50,5 +70,61 @@ public class MultipleDaysWeather {
 
   public void setMaxTemp(double maxTemp) {
     this.maxTemp = maxTemp;
+  }
+
+  @NonNull
+  @Override
+  public MyViewHolder getViewHolder(@NonNull View v) {
+    return new MyViewHolder(v);
+  }
+
+  @Override
+  public int getType() {
+    return R.id.fastadapter_item_adapter;
+  }
+
+  @Override
+  public int getLayoutRes() {
+    return R.layout.multiple_days_item;
+  }
+
+  protected static class MyViewHolder extends FastAdapter.ViewHolder<MultipleDaysWeather> {
+    Context context;
+    View view;
+    @BindView(R.id.day_name_text_view)
+    AppCompatTextView dayNameTextView;
+    @BindView(R.id.date_text_view)
+    AppCompatTextView dateTextView;
+    @BindView(R.id.weather_image_view)
+    AppCompatImageView weatherImageView;
+    @BindView(R.id.max_temp_text_view)
+    AppCompatTextView maxTempTextView;
+    @BindView(R.id.min_temp_text_view)
+    AppCompatTextView minTempTextView;
+
+    MyViewHolder(View view) {
+      super(view);
+      ButterKnife.bind(this, view);
+      this.view = view;
+      this.context = view.getContext();
+    }
+
+    @Override
+    public void bindView(@NonNull MultipleDaysWeather item, @NonNull List<Object> payloads) {
+      Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+      calendar.setTimeInMillis(item.getDt() * 1000L);
+      dayNameTextView.setText(Constants.DAYS_OF_WEEK[calendar.get(Calendar.DAY_OF_WEEK) - 1]);
+      dateTextView.setText(String.format(Locale.getDefault(), "%s %d",
+          Constants.MONTH_NAME[calendar.get(Calendar.MONTH)], calendar.get(Calendar.DAY_OF_MONTH)));
+      minTempTextView.setText(String.format(Locale.getDefault(), "%.0f°", item.getMinTemp()));
+      maxTempTextView.setText(String.format(Locale.getDefault(), "%.0f°", item.getMaxTemp()));
+      AppUtil.setWeatherIcon(context, weatherImageView, item.getWeatherId());
+    }
+
+    @Override
+    public void unbindView(@NonNull MultipleDaysWeather item) {
+
+    }
+
   }
 }
