a95638640a4f0fe13a6143d4780d2b629932219d
Behrouz <bkhezry@gmail.com>
2019-05-14 12:42:57 +0430

    add layout of about fragment. showing basic information about application.

--- app/src/main/java/com/github/bkhezry/weather/ui/fragment/AboutFragment.java
+++ app/src/main/java/com/github/bkhezry/weather/ui/fragment/AboutFragment.java
@@ -1,17 +1,20 @@
 package com.github.bkhezry.weather.ui.fragment;
 
 import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 import com.github.bkhezry.weather.R;
+import com.github.bkhezry.weather.utils.AppUtil;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -24,7 +27,21 @@ public class AboutFragment extends DialogFragment {
     View view = inflater.inflate(R.layout.fragment_about,
         container, false);
     ButterKnife.bind(this, view);
+    String versionName = "";
+    try {
+      versionName = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName;
+    } catch (PackageManager.NameNotFoundException e) {
+      // do nothing
+    }
+    setTextWithLinks(view.findViewById(R.id.text_application_info), getString(R.string.application_info_text, versionName));
+    setTextWithLinks(view.findViewById(R.id.text_developer_info), getString(R.string.developer_info_text));
+    setTextWithLinks(view.findViewById(R.id.text_libraries), getString(R.string.libraries_text));
+    setTextWithLinks(view.findViewById(R.id.text_license), getString(R.string.license_text));
     return view;
+  }
+
+  private void setTextWithLinks(TextView textView, String htmlText) {
+    AppUtil.setTextWithLinks(textView, AppUtil.fromHtml(htmlText));
   }
 
 
--- app/src/main/java/com/github/bkhezry/weather/utils/AppUtil.java
+++ app/src/main/java/com/github/bkhezry/weather/utils/AppUtil.java
@@ -1,12 +1,21 @@
 package com.github.bkhezry.weather.utils;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
+import android.text.Html;
+import android.text.Layout;
+import android.text.Spannable;
+import android.text.TextUtils;
+import android.text.style.ClickableSpan;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.AppCompatImageView;
@@ -160,5 +169,74 @@ public class AppUtil {
       }
     });
     dialog.show();
+  }
+
+  @SuppressLint("ClickableViewAccessibility")
+  public static void setTextWithLinks(TextView textView, CharSequence html) {
+    textView.setText(html);
+    textView.setOnTouchListener(new View.OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction();
+        if (action == MotionEvent.ACTION_UP ||
+            action == MotionEvent.ACTION_DOWN) {
+          int x = (int) event.getX();
+          int y = (int) event.getY();
+
+          TextView widget = (TextView) v;
+          x -= widget.getTotalPaddingLeft();
+          y -= widget.getTotalPaddingTop();
+
+          x += widget.getScrollX();
+          y += widget.getScrollY();
+
+          Layout layout = widget.getLayout();
+          int line = layout.getLineForVertical(y);
+          int off = layout.getOffsetForHorizontal(line, x);
+
+          ClickableSpan[] link = Spannable.Factory.getInstance()
+              .newSpannable(widget.getText())
+              .getSpans(off, off, ClickableSpan.class);
+
+          if (link.length != 0) {
+            if (action == MotionEvent.ACTION_UP) {
+              link[0].onClick(widget);
+            }
+            return true;
+          }
+        }
+        return false;
+      }
+    });
+  }
+
+  public static CharSequence fromHtml(String htmlText) {
+    return fromHtml(htmlText, false);
+  }
+
+  public static CharSequence fromHtml(String htmlText, boolean compact) {
+    if (TextUtils.isEmpty(htmlText)) {
+      return null;
+    }
+    CharSequence spanned;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+      spanned = Html.fromHtml(htmlText, compact ?
+          Html.FROM_HTML_MODE_COMPACT : Html.FROM_HTML_MODE_LEGACY);
+    } else {
+      //noinspection deprecation
+      spanned = Html.fromHtml(htmlText);
+    }
+    return trim(spanned);
+  }
+
+  private static CharSequence trim(CharSequence charSequence) {
+    if (TextUtils.isEmpty(charSequence)) {
+      return charSequence;
+    }
+    int end = charSequence.length() - 1;
+    while (Character.isWhitespace(charSequence.charAt(end))) {
+      end--;
+    }
+    return charSequence.subSequence(0, end + 1);
   }
 }
