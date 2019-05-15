a6ee739de97d8235f9fa7bc91671193c06d0f8e2
Behrouz <bkhezry@gmail.com>
2019-05-16 01:30:19 +0430

    add configure for handle multiple language in MyApplication class.

--- app/src/main/java/com/github/bkhezry/weather/utils/MyApplication.java
+++ app/src/main/java/com/github/bkhezry/weather/utils/MyApplication.java
@@ -1,6 +1,8 @@
 package com.github.bkhezry.weather.utils;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.res.Configuration;
 
 import com.github.bkhezry.weather.BuildConfig;
 import com.github.bkhezry.weather.model.db.MyObjectBox;
@@ -10,6 +12,7 @@ import io.objectbox.android.AndroidObjectBrowser;
 
 public class MyApplication extends Application {
   private static BoxStore boxStore;
+  public static LocaleManager localeManager;
 
   public static BoxStore getBoxStore() {
     return boxStore;
@@ -26,5 +29,17 @@ public class MyApplication extends Application {
     if (BuildConfig.DEBUG) {
       new AndroidObjectBrowser(boxStore).start(this);
     }
+  }
+
+  @Override
+  protected void attachBaseContext(Context base) {
+    localeManager = new LocaleManager(base);
+    super.attachBaseContext(localeManager.setLocale(base));
+  }
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    localeManager.setLocale(this);
   }
 }
