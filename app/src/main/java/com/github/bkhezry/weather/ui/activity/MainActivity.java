0a616eedf687116826a37ad85944337a4812ced8
Behrouz <bkhezry@gmail.com>
2019-05-25 02:35:53 +0430

    show cursor in search view.

--- app/src/main/java/com/github/bkhezry/weather/ui/activity/MainActivity.java
+++ app/src/main/java/com/github/bkhezry/weather/ui/activity/MainActivity.java
@@ -147,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
   private void initSearchView() {
     searchView.setVoiceSearch(false);
     searchView.setHint(getString(R.string.search_label));
-    searchView.setCursorDrawable(R.drawable.ic_action_action_search);
+    searchView.setCursorDrawable(R.drawable.custom_curosr);
     searchView.setEllipsize(true);
     searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
       @Override
